81a0c352ae3a83f292439cc142438385eb743172
Albert <59813595+justalbertcode@users.noreply.github.com>
2022-06-13 15:39:43 +0600

    Update script.js

--- script.js
+++ script.js
@@ -7,15 +7,18 @@ const clearbtn = document.querySelector('.clearBtn');
 
 
 btn.addEventListener('click', () => {
-   
+   //get input value
     let outText = input.value;
-    
+   
+   //push input value to the out-div
     out.innerHTML += outText + `<br/>`;
   
+   //clear input
     input.value = "";
   
 });
 
+//clear btn
 clearbtn.addEventListener('click', () => {
    
     input.value = "";
